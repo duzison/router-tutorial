8c4891aa43a3defdfcb5205c85519b219f0868c2
sojin.son <sojin.son96@gmail.com>
2022-12-13 22:41:21 +0900

    13 리액트 라우터로 SPA 개발하기

---
+++ src/Layout.js
@@ -0,0 +1,29 @@
+import {Outlet, useNavigate, usenavigate} from 'react-router-dom';
+
+const Layout = () => {
+    const navigate = useNavigate();
+
+    const goBack = () => {
+        //이전페이지로 이동
+        navigate(-1);
+    };
+
+    const goArticles = () => {
+        //articles 경로로 이동
+        navigate('/articles', {replace : true});
+    }
+
+    return (
+        <div>
+            <header style={{background:'lightgray', padding:15, fontsize:24}}>
+                <button onClick={goBack}>뒤로가기</button>
+                <button onClick={goArticles}>게시글 목록</button>
+            </header>
+            <main>
+                <Outlet />
+            </main>
+        </div>
+    );
+};
+
+export default Layout;
---
+++ src/pages/About.js
@@ -0,0 +1,28 @@
+import {useLocation, useSearchParams} from 'react-router-dom';
+
+const About = () => {
+    const [searchParams, setSearchPrams] =useSearchParams();
+    const detail = searchParams.get('detail');
+    const mode = searchParams.get('mode');
+    
+    const onToggleDetail = () => {
+        setSearchPrams({mode,detail: detail === 'true' ? false : true});
+    };
+    const onIncreaseMode = () => {
+        const nextMode = mode===null ? 1 : parseInt(mode) + 1;
+        setSearchPrams({mode:nextMode, detail});
+    }
+    return(
+        <div>
+            <h1>소개</h1>
+            <p>리액트 라우터를 사용해 보는 프로젝트입니다.</p>
+            {/* <p>쿼리스트링: {location.search}</p> */}
+            <p>detail:{detail}</p>
+            <p>mode:{mode}</p>
+            <button onClick = {onToggleDetail}>Toggle detail</button>
+            <button onClick = {onIncreaseMode}>mode+1</button>
+        </div>
+    );
+};
+
+export default About;
